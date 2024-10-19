a210fd0c29bc09ba4dac9fec1f28c2ba29bc36eb
Kiran Rajput <rajputkiran2805@gmail.com>
2024-10-19 20:33:38 +0530

    Delete HotelService directory

--- HotelService/src/main/java/com/guardians/hotel/service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.guardians.hotel.service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- HotelService/src/main/java/com/guardians/hotel/service/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.guardians.hotel.service.repository;
-
-import com.guardians.hotel.service.enitites.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel,String> {
-
-}
